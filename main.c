40b899b95fbf81abf0b7e4e5be8fe7ce201c2e8d
Max <vvmax01@mail.ru>
2022-03-12 10:56:00 +0300

    Working short! ls

--- main.c
+++ main.c
@@ -15,6 +15,8 @@ const int STATUS_ERROR_INPUT_PARAM = 0;
 
 int readDir(char *dirName);
 
+int ls(char *str);
+
 int main(int argc, char *argv[]) {
     int status;
     status = readDir(argv[1]);
@@ -46,6 +48,7 @@ int readDir(char *dirName) {
     size_t len = strlen(dirName);
     strncpy(buffer, dirName, len);
     printf("%s\n", buffer);
+    ls(buffer);
     while ((dp = readdir(pDir)) != NULL) {
         if (strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0) {
             continue;
@@ -61,42 +64,15 @@ int readDir(char *dirName) {
     return STATUS_OK;
 }
 
-int ls_long(char *str, char *argv) {
+int ls(char *str) {
     DIR *dirp;
     struct dirent *dp;
-    printf("***%s***\n", str);
     dirp = opendir(str);
-
-
-    if (readdir(dirp) == NULL) {
-        return -1;
-    }
-    chdir(str);
     while ((dp = readdir(dirp)) != NULL) {
-        if (dp->d_name[0] == '.') continue;
-        struct stat buf;
-        int st;
-        st = stat(dp->d_name, &buf);
-        if (st == -1)
-            return -1;
-        char type = '-';
-        if ((buf.st_mode & S_IFDIR) == S_IFDIR) type = 'd';
-        if (type == 'd' && (dp->d_name[0] != '.')) {
-            char *str1 = str;
-            if (str[strlen(str) - 1] != '/') str1 = concat(str1, "/");
-            str1 = concat(str1, dp->d_name);
-            printf("||%s %s||", str1, argv);
-            printf("||%s %s||", str, argv);
-            //   ls_long(str1,argv);
+        if (strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0) {
+            continue;
         }
-        int j;
-
-
-        // get time
-        struct tm *t;
-        t = localtime(&buf.st_mtime);
-        printf("%c \t%ld  \t%ld \t%02dmonth%02d%02d:%02d \t%s \n", type, buf.st_nlink, buf.st_size, t->tm_mon + 1,
-               t->tm_mday, t->tm_hour, t->tm_min, dp->d_name);
+        printf("%s/%s\n", str, dp->d_name);
     }
     closedir(dirp);
     return 1;
